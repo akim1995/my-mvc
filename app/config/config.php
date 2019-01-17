2efe40ae9ee150ff62a2f879e7dda2a0122426bf
akim1995 <a.khalitov@clientlab.ru>
2019-01-17 14:31:49 +0200

    removed / from urlroot const

--- app/config/config.php
+++ app/config/config.php
@@ -10,7 +10,7 @@ define('APPROOT', $_SERVER['DOCUMENT_ROOT'] . '/app');
 	// URL Root
 // you can also hardcode url root like http://my-mvc.lan/
 define('URLROOT', $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . 
-	'://' . $_SERVER['HTTP_HOST'] . '/');
+	'://' . $_SERVER['HTTP_HOST']);
 	// Site Name
 define('SITENAME', 'your_sitename');
 
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -3,6 +3,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo SITENAME . ($data['title'] ? ' | ' .$data['title'] : '') ?></title>
-	<link rel="stylesheet" href="<?php echo URLROOT;?>css/style.css">
+	<link rel="stylesheet" href="<?php echo URLROOT;?>/css/style.css">
 </head>
 <body>
